981ffaf8d7ebb725755b4bb4b683a089ab7942c9
Alexandre Kozoubsky <akozoubsky@gmail.com>
2015-06-03 22:58:00 -0300

    Comiit de segurança para estao fazer merge

--- admin-setup.php
+++ admin-setup.php
@@ -29,9 +29,9 @@
  * Remove WordPress Icon from Admin Bar
  */
  
-add_action( 'wp_before_admin_bar_render', 'adms_remove_admin_bar_links' );
+add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_links' );
 
-function adms_remove_admin_bar_links() {
+function remove_admin_bar_links() {
 	 global $wp_admin_bar;
 	$wp_admin_bar->remove_menu('wp-logo');
 }
@@ -46,9 +46,9 @@ add_filter('show_admin_bar', '__return_false');
  * Admin footer modification.
  */
 
-add_filter('admin_footer_text', 'adms_remove_footer_admin');
+add_filter('admin_footer_text', 'remove_footer_admin');
 
-function adms_remove_footer_admin () {
+function remove_footer_admin () {
     echo '<span id="footer-thankyou">Desenvolvido por <a href="http://www.alexandrekozoubsky.com" target="_blank">Alexandre Kozoubsky - http://www.alexandrekozoubsky.com</a></span>';
 }
 
@@ -59,9 +59,9 @@ function adms_remove_footer_admin () {
  * Removing a menu does not replace the need to filter a user's permissions as appropriate. 
  */
 
-add_action( 'admin_menu', 'adms_remove_menu_pages' );
+add_action( 'admin_menu', 'ak_remove_menu_pages' );
 
-function adms_remove_menu_pages() {
+function ak_remove_menu_pages() {
     
 	if ( ! current_user_can( 'activate_plugins' ) ) {
 	
@@ -75,9 +75,9 @@ function adms_remove_menu_pages() {
  * Remove widgets from the Admin Dashboard screen.
  */
 
-add_action('wp_dashboard_setup', 'adms_remove_admin_dashboard_widgets' );
+add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
 
-function adms_remove_admin_dashboard_widgets() {
+function remove_dashboard_widgets() {
 
 	// Remove meta boxes from Wordpress dashboard for all users
 	remove_meta_box('dashboard_primary', 'dashboard', 'side');   // WordPress blog
@@ -88,20 +88,45 @@ function adms_remove_admin_dashboard_widgets() {
 		// Globalize the metaboxes array, this holds all the widgets for wp-admin
 		global $wp_meta_boxes;
      
-		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
-		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
+		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
+		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
-		unset($wp_meta_boxes['dashboard']['normal']['core']['photocrati_admin_dashboard_widget']); 
+		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_drafts']);
+		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
+		unset($wp_meta_boxes['dashboard']['normal']['core']['photocrati_admin_dashboard_widget']);
+		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
+		unset($wp_meta_boxes['dashboard']['normal']['core']['yoast_db_widget']);
+		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_addthis']);
+		unset($wp_meta_boxes['dashboard']['normal']['core']['events_dashboard_window']);	
+		unset($wp_meta_boxes['dashboard']['normal']['core']['bbp-dashboard-right-now']); //BBPress
+		unset($wp_meta_boxes['dashboard']['normal']['core']['rg_forms_dashboard']); // Gravity Forms	
     
-		remove_meta_box('dashboard_right_now', 'dashboard', 'normal');   // Right Now
-		/* remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal'); */ // Recent Comments
-		remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');  // Incoming Links
-		remove_meta_box('dashboard_plugins', 'dashboard', 'normal');   // Plugins
-		remove_meta_box('dashboard_quick_press', 'dashboard', 'side');  // Quick Press
-		remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');  // Recent Drafts
-		// use 'dashboard-network' as the second parameter to remove widgets from a network dashboard.    
+		//remove_menu_page('index.php'); // Dashboard
+		//remove_menu_page('edit.php'); // Posts
+		remove_menu_page('upload.php'); // Media
+		remove_menu_page('link-manager.php'); // Links
+		//remove_menu_page('edit.php?post_type=page'); // Pages
+		//remove_menu_page('edit-comments.php'); // Comments
+		remove_menu_page('themes.php'); // Appearance
+		remove_menu_page('plugins.php'); // Plugins
+		remove_menu_page('users.php'); // Users
+		remove_menu_page('tools.php'); // Tools
+		remove_menu_page('options-general.php'); // Settings
+		remove_menu_page('itsec'); // IThemes Security	
+		remove_menu_page('duplicator'); // Duplicator
     
 	}
+	
+	/*
+	if ( ! current_user_can( 'edit_tribe_event' ) ) {
+		remove_menu_page('edit.php?post_type=tribe_events'); // Tribe Events - Calendar
+	}
+	*/
+	
+	add_filter('pre_option_update_core','__return_null');
+	add_filter('pre_site_transient_update_core','remove_core_updates');
+	add_filter('pre_site_transient_update_plugins','remove_core_updates');
+	add_filter('pre_site_transient_update_themes','remove_core_updates');
 	 
 }
 
