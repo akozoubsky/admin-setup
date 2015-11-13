d04b966b78f55a657c1a6b57771c465c6b2f3f08
Alexandre Kozoubsky <akozoubsky@gmail.com>
2015-11-12 23:27:11 -0200

    Code cleaning.

--- admin-setup.php
+++ admin-setup.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name: Admin Setup
  * Plugin URI: https://github.com/akozoubsky/admin-setup
- * Description: Setup WP Admin. 
- * Version: 0.0.1
+ * Description: Template to show how we can change/hide menu itens and dashboard itens in the WordPress Admin. It is only a case, you should use plugins like Members (https://wordpress.org/plugins/members/) and User Role Editor (https://wordpress.org/plugins/user-role-editor/) instead.
+ * Version: 0.0.2
  * Author: Alexandre Kozoubsky
  * Author URI: http://alexandrekozoubsky.com
  *
@@ -29,9 +29,9 @@
  * Remove WordPress Icon from Admin Bar
  */
  
-add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_links' );
+add_action( 'wp_before_admin_bar_render', 'adminsetup_remove_admin_bar_links' );
 
-function remove_admin_bar_links() {
+function adminsetup_remove_admin_bar_links() {
 	 global $wp_admin_bar;
 	$wp_admin_bar->remove_menu('wp-logo');
 }
@@ -46,10 +46,10 @@ add_filter('show_admin_bar', '__return_false');
  * Admin footer modification.
  */
 
-add_filter('admin_footer_text', 'remove_footer_admin');
+add_filter('admin_footer_text', 'adminsetup_remove_footer_admin');
 
-function remove_footer_admin () {
-    echo '<span id="footer-thankyou">Desenvolvido por <a href="http://www.alexandrekozoubsky.com" target="_blank">Alexandre Kozoubsky - http://www.alexandrekozoubsky.com</a></span>';
+function adminsetup_remove_footer_admin () {
+    echo '<span id="footer-thankyou">Desenvolvido por <a href="http://www.alexandrekozoubsky.com.br" target="_blank">Alexandre Kozoubsky - http://alexandrekozoubsky.com.br</a></span>';
 }
 
 /**
@@ -59,14 +59,29 @@ function remove_footer_admin () {
  * Removing a menu does not replace the need to filter a user's permissions as appropriate. 
  */
 
-add_action( 'admin_menu', 'ak_remove_menu_pages' );
+add_action( 'admin_menu', 'adminsetup_remove_menu_pages' );
 
-function ak_remove_menu_pages() {
+function adminsetup_remove_menu_pages() {
     
-	if ( ! current_user_can( 'activate_plugins' ) ) {
-	
+	if ( ! current_user_can( 'manage_options' ) ) {
+
+		/* Pick up your itens */
+		remove_menu_page('index.php'); // Panel Dashboard		
 		remove_menu_page('options-general.php');
 		remove_menu_page('tools.php');
+		remove_menu_page('edit.php'); // Posts
+		remove_menu_page('upload.php'); // Media
+		remove_menu_page('link-manager.php'); // Links
+		remove_menu_page('edit.php?post_type=page'); // Pages
+		remove_menu_page('edit-comments.php'); // Comments
+		remove_menu_page('themes.php'); // Appearance
+		remove_menu_page('plugins.php'); // Plugins
+		remove_menu_page('users.php'); // Users
+		remove_menu_page('tools.php'); // Tools
+		remove_menu_page('options-general.php'); // Settings
+		remove_menu_page('itsec'); // IThemes Security Plugin
+		remove_menu_page('duplicator'); // Duplicator Plugin
+		remove_menu_page('flamingo'); // Flamingo Plugin		
 	}
       
 }
@@ -75,55 +90,38 @@ function ak_remove_menu_pages() {
  * Remove widgets from the Admin Dashboard screen.
  */
 
-add_action('wp_dashboard_setup', 'remove_dashboard_widgets' );
+add_action('wp_dashboard_setup', 'adminsetup_remove_dashboard_widgets' );
 
-function remove_dashboard_widgets() {
+function adminsetup_remove_dashboard_widgets() {
 
 	// Remove meta boxes from Wordpress dashboard for all users
 	remove_meta_box('dashboard_primary', 'dashboard', 'side');   // WordPress blog
 	remove_meta_box('dashboard_secondary', 'dashboard', 'side');   // Other WordPress News
 		
-	if ( ! current_user_can( 'activate_plugins' ) ) {
+	if ( ! current_user_can( 'manage_options' ) ) {
 	
 		// Globalize the metaboxes array, this holds all the widgets for wp-admin
 		global $wp_meta_boxes;
-     
+
+		/* Pick up your itens */     
 		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_drafts']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
-		unset($wp_meta_boxes['dashboard']['normal']['core']['photocrati_admin_dashboard_widget']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
-		unset($wp_meta_boxes['dashboard']['normal']['core']['yoast_db_widget']);
-		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_addthis']);
-		unset($wp_meta_boxes['dashboard']['normal']['core']['events_dashboard_window']);	
+		unset($wp_meta_boxes['dashboard']['normal']['core']['photocrati_admin_dashboard_widget']); // Photocrati		
+		unset($wp_meta_boxes['dashboard']['normal']['core']['yoast_db_widget']); // Yoast
+		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_addthis']); // AddThis
+		unset($wp_meta_boxes['dashboard']['normal']['core']['events_dashboard_window']); // The Events Calendar	
 		unset($wp_meta_boxes['dashboard']['normal']['core']['bbp-dashboard-right-now']); //BBPress
 		unset($wp_meta_boxes['dashboard']['normal']['core']['rg_forms_dashboard']); // Gravity Forms	
-    
-		//remove_menu_page('index.php'); // Dashboard
-		//remove_menu_page('edit.php'); // Posts
-		remove_menu_page('upload.php'); // Media
-		remove_menu_page('link-manager.php'); // Links
-		//remove_menu_page('edit.php?post_type=page'); // Pages
-		//remove_menu_page('edit-comments.php'); // Comments
-		remove_menu_page('themes.php'); // Appearance
-		remove_menu_page('plugins.php'); // Plugins
-		remove_menu_page('users.php'); // Users
-		remove_menu_page('tools.php'); // Tools
-		remove_menu_page('options-general.php'); // Settings
-		remove_menu_page('itsec'); // IThemes Security	
-		remove_menu_page('duplicator'); // Duplicator
-    
 	}
-	
 
 	/* 
 	if ( ! current_user_can( 'edit_tribe_event' ) ) {
 		remove_menu_page('edit.php?post_type=tribe_events'); // Tribe Events - Calendar
 	}
 	*/
-	 
 }
-
 ?>
