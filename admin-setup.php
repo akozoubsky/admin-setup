870ac64ad22325196002447bb917bfa3263eaf0d
Alexandre Kozoubsky <akozoubsky@gmail.com>
2016-04-07 02:41:51 -0300

    Modify rights;

--- admin-setup.php
+++ admin-setup.php
@@ -66,12 +66,12 @@ function adminsetup_remove_menu_pages() {
 	if ( ! current_user_can( 'manage_options' ) ) {
 
 		/* Pick up your itens */
-		remove_menu_page('index.php'); // Panel Dashboard		
-		remove_menu_page('edit.php'); // Posts
-		remove_menu_page('upload.php'); // Media
+		//remove_menu_page('index.php'); // Panel Dashboard		
+		//remove_menu_page('edit.php'); // Posts
+		//remove_menu_page('upload.php'); // Media
 		remove_menu_page('link-manager.php'); // Links
-		remove_menu_page('edit.php?post_type=page'); // Pages
-		remove_menu_page('edit-comments.php'); // Comments
+		//remove_menu_page('edit.php?post_type=page'); // Pages
+		//remove_menu_page('edit-comments.php'); // Comments
 		remove_menu_page('themes.php'); // Appearance
 		remove_menu_page('plugins.php'); // Plugins
 		remove_menu_page('users.php'); // Users
