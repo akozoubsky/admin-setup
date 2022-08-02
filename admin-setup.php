4c39764d7738492f4629aae470f38a919412fc4b
Alexandre Kozoubsky <akozoubsky@gmail.com>
2022-08-02 18:25:11 -0300

    Inclusão de metaboxes

--- admin-setup.php
+++ admin-setup.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Admin Setup
  * Plugin URI: https://github.com/akozoubsky/admin-setup
- * Description: Template to show how we can change/hide menu itens and dashboard itens in the WordPress Admin. It is only a case, you should use plugins like Members (https://wordpress.org/plugins/members/) and User Role Editor (https://wordpress.org/plugins/user-role-editor/) instead.
+ * Description: Setup WP Admin. 
  * Version: 0.0.2
  * Author: Alexandre Kozoubsky
  * Author URI: http://alexandrekozoubsky.com
@@ -29,9 +29,9 @@
  * Remove WordPress Icon from Admin Bar
  */
  
-add_action( 'wp_before_admin_bar_render', 'adminsetup_remove_admin_bar_links' );
+add_action( 'wp_before_admin_bar_render', 'remove_admin_bar_links' );
 
-function adminsetup_remove_admin_bar_links() {
+function remove_admin_bar_links() {
 	 global $wp_admin_bar;
 	$wp_admin_bar->remove_menu('wp-logo');
 }
@@ -40,7 +40,7 @@ function adminsetup_remove_admin_bar_links() {
  * Leave the Toolbar available in the Dashboard but hide it on all front facing pages.
  */
 
-add_filter('show_admin_bar', '__return_false');
+//add_filter('show_admin_bar', '__return_false');
 
 /**
  * Admin footer modification.
@@ -49,7 +49,7 @@ add_filter('show_admin_bar', '__return_false');
 add_filter('admin_footer_text', 'adminsetup_remove_footer_admin');
 
 function adminsetup_remove_footer_admin () {
-    echo '<span id="footer-thankyou">Desenvolvido por <a href="http://www.alexandrekozoubsky.com.br" target="_blank">Alexandre Kozoubsky - http://alexandrekozoubsky.com.br</a></span>';
+    echo '<span id="footer-thankyou">Desenvolvido por <a href="http://www.alexandrekozoubsky.com" target="_blank">Alexandre Kozoubsky - http://www.alexandrekozoubsky.com</a></span>';
 }
 
 /**
@@ -59,66 +59,112 @@ function adminsetup_remove_footer_admin () {
  * Removing a menu does not replace the need to filter a user's permissions as appropriate. 
  */
 
-add_action( 'admin_menu', 'adminsetup_remove_menu_pages', 999 ); // Priority is important for some plugins like ACF
+add_action( 'admin_menu', 'adminsetup_remove_menu_pages' );
 
 function adminsetup_remove_menu_pages() {
     
-	if ( ! current_user_can( 'manage_options' ) ) {
-
-		/* Pick up your itens */
-		//remove_menu_page('index.php'); // Panel Dashboard		
-		//remove_menu_page('edit.php'); // Posts
-		//remove_menu_page('upload.php'); // Media
-		remove_menu_page('link-manager.php'); // Links
-		//remove_menu_page('edit.php?post_type=page'); // Pages
-		//remove_menu_page('edit-comments.php'); // Comments
-		remove_menu_page('themes.php'); // Appearance
-		remove_menu_page('plugins.php'); // Plugins
-		remove_menu_page('users.php'); // Users
-		remove_menu_page('tools.php'); // Tools
-		remove_menu_page('options-general.php'); // Settings
-		
-		remove_menu_page('itsec'); // IThemes Security Plugin
-		remove_menu_page('duplicator'); // Duplicator Plugin
-		remove_menu_page('flamingo'); // Flamingo Plugin
-		remove_menu_page('edit.php?post_type=acf'); // Advanced Custom Fields Plugin	
-		remove_menu_page('acf-options'); // ACF Options Page Plugin
-		remove_menu_page('jetpack'); // JetPack - Obs: Soh funciona com prioridade muito alta
+	if ( ! current_user_can( 'activate_plugins' ) ) {
+	
+		remove_menu_page('options-general.php');
+		remove_menu_page('tools.php');
 	}
       
 }
 
 /**
  * Remove widgets from the Admin Dashboard screen.
+ * @link https://gist.github.com/chrisguitarguy/1377965
  */
 
-add_action('wp_dashboard_setup', 'adminsetup_remove_dashboard_widgets', 999 );
+add_action('wp_dashboard_setup', 'adminsetup_remove_dashboard_widgets' );
 
 function adminsetup_remove_dashboard_widgets() {
 
 	// Remove meta boxes from Wordpress dashboard for all users
-	remove_meta_box('dashboard_primary', 'dashboard', 'side');   // WordPress blog
-	remove_meta_box('dashboard_secondary', 'dashboard', 'side');   // Other WordPress News
+
+	/**
+	 * Removes the "Right Now" widget that tells you post/comment counts
+	 * and what theme you're using.
+	 */
+	remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+	
+	/**
+	 * Removes the recent comments widget
+	 */
+	remove_meta_box( 'dashboard_recent_comments', 'dashboard', 'normal' );
+	
+	/**
+	 * Removes the incoming links widget.
+	 */
+	remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
+	
+	/**
+	 * Removes the plugins widgets that displays the most popular,
+	 * newest, and recently updated plugins
+	 */
+	remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );
+	
+	/**
+	 * Removes the quick press widget that allows you post right from the dashboard
+	 */
+	remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
+	
+	/**
+	 * Removes the widget containing the list of recent drafts
+	 */
+	remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );
+	
+	/**
+	 * Removes the "WordPress Blog" widget
+	 */
+	remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
+	
+	/**
+	 * Removes the "Other WordPress News" widget
+	 */
+	remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );
+	
+	/**
+	 * Removes the "Site Health" widget
+	 */
+	remove_meta_box('dashboard_site_health', 'dashboard', 'normal');	
+
 		
-	if ( ! current_user_can( 'manage_options' ) ) {
+	if ( ! current_user_can( 'activate_plugins' ) ) {
 	
 		// Globalize the metaboxes array, this holds all the widgets for wp-admin
 		global $wp_meta_boxes;
-
-		/* Pick up your itens */     
+     
 		unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_drafts']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
+		unset($wp_meta_boxes['dashboard']['normal']['core']['photocrati_admin_dashboard_widget']);
 		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
-		unset($wp_meta_boxes['dashboard']['normal']['core']['photocrati_admin_dashboard_widget']); // Photocrati		
-		unset($wp_meta_boxes['dashboard']['normal']['core']['yoast_db_widget']); // Yoast
-		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_addthis']); // AddThis
-		unset($wp_meta_boxes['dashboard']['normal']['core']['events_dashboard_window']); // The Events Calendar	
+		unset($wp_meta_boxes['dashboard']['normal']['core']['yoast_db_widget']);
+		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_addthis']);
+		unset($wp_meta_boxes['dashboard']['normal']['core']['events_dashboard_window']);	
 		unset($wp_meta_boxes['dashboard']['normal']['core']['bbp-dashboard-right-now']); //BBPress
 		unset($wp_meta_boxes['dashboard']['normal']['core']['rg_forms_dashboard']); // Gravity Forms
-		unset($wp_meta_boxes['dashboard']['normal']['core']['jetpack_summary_widget']); // JetPack
+		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_quick_press']);  // Rascunho rápido
+		unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']);  // Atividade
+		unset($wp_meta_boxes['dashboard']['normal']['core']['wp_dashboard_events_news']);  // Atividade			
+		
+		//remove_menu_page('index.php'); // Dashboard
+		//remove_menu_page('edit.php'); // Posts
+		//remove_menu_page('upload.php'); // Media
+		remove_menu_page('link-manager.php'); // Links
+		//remove_menu_page('edit.php?post_type=page'); // Pages
+		//remove_menu_page('edit-comments.php'); // Comments
+		remove_menu_page('themes.php'); // Appearance
+		//remove_menu_page('plugins.php'); // Plugins
+		//remove_menu_page('users.php'); // Users
+		remove_menu_page('tools.php'); // Tools
+		remove_menu_page('options-general.php'); // Settings
+		remove_menu_page('itsec'); // IThemes Security	
+		remove_menu_page('duplicator'); // Duplicator
+    
 	}
 
 	/* 
